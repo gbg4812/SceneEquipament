d2a8dbca8d645d90893fde20b0cabcb6f61a485b
Guillem Baldi <guillembaldi@gmail.com>
2026-01-13 19:28:17 +0100

    Resourcesssss

--- src/Resource.hpp
+++ src/Resource.hpp
@@ -1,19 +1,19 @@
 
-
 #include <string>
+
 class Resource {
    public:
-    Resource(std::string name, uint32_t rid) : _name(name), _rid(rid) {}
-    Resource(Resource &&) = default;
-    Resource(const Resource &) = default;
-    Resource &operator=(Resource &&) = default;
-    Resource &operator=(const Resource &) = default;
+    Resource(std::string name, uint rid) : _name(name), _rid(rid) {}
+    Resource(Resource&&) = default;
+    Resource(const Resource&) = default;
+    Resource& operator=(Resource&&) = default;
+    Resource& operator=(const Resource&) = default;
     ~Resource() {}
 
-    const std::string &getName() const { return _name; }
-    const uint32_t &getRID() const { return _rid; }
+    const std::string& getName() const { return _name; }
+    const uint& getRID() const { return _rid; }
 
    private:
     std::string _name;
-    uint32_t _rid;
+    uint _rid;
 };
---
+++ tests/main.cpp
@@ -0,0 +1,6 @@
+#include <print>
+
+int main(int argc, char* argv[]) {
+    std::print("%i", 10);
+    return 0;
+}
